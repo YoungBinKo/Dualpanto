1aa998b45eb03f364ee17819126017c3a5ae4792
YoungBinKo <50771294+YoungBinKo@users.noreply.github.com>
2020-03-18 11:18:53 +0100

    basic game is done

---
+++ catching mole/Assets/Script/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))//0:left mouse button, 1:right mouse button 2:middle
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//creat ray
+            //Ray ScreenPointToRay(Vector3 position) : Return a ray going from camera through a screen point.
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))//physics check
+            {
+                if(hit.collider.tag == "Mole") 
+                {
+                    MoleBehavior mole = hit.collider.gameObject.GetComponent<MoleBehavior>();
+                    //get the MoleBehavior from the Mole, that i hit
+
+                    mole.SwitchCollider(0);//turning off
+
+                    mole.anim.SetTrigger("hit");//using Trigger for calling animation
+
+                    //Debug.Log(hit.collider.gameObject + " got hit ");//check what we hit Mole
+                }
+                
+            }
+        }
+    }
+}
---
+++ catching mole/Assets/Script/MoleBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleBehavior : MonoBehaviour
+{
+    Collider col;
+    public int hitPoints = 1;
+
+    [HideInInspector]public Animator anim;//to have animator component conector
+    //HideInInspector makes a variable not show up in the inspector but be serialized
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        col = GetComponent<Collider>();//get the Component from gameObj which is type of Collider
+        col.enabled = false;//this collider will be disable by start
+    }
+    public void DestroyObj()
+    {
+        Destroy(gameObject);
+    }
+    public void SwitchCollider(int num)
+    {
+        col.enabled = (num == 0) ? false : true;
+    }
+
+    //FOR hitPOINTS
+    public void GotHit()
+    {
+        hitPoints--;
+
+        if(hitPoints > 0)
+        {
+            col.enabled = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
